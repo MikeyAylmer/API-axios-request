c089801a67cee4783ead15255b71407bc0324a04
Michael Aylmer <119439778+MikeyAylmer@users.noreply.github.com>
2023-05-31 12:33:50 -0400

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,4 @@
-"use strict";
 
-/******************************************************************************
- * Handling navbar clicks and updating navbar
- */
-
-/** Show main list of all stories when click site name */
 
 function navAllStories(evt) {
   console.debug("navAllStories", evt);
@@ -14,7 +8,6 @@ function navAllStories(evt) {
 
 $body.on("click", "#nav-all", navAllStories);
 
-/** Show login/signup on click on "login" */
 
 function navLoginClick(evt) {
   console.debug("navLoginClick", evt);
@@ -25,7 +18,7 @@ function navLoginClick(evt) {
 
 $navLogin.on("click", navLoginClick);
 
-/** When a user first logins in, update the navbar to reflect that. */
+
 
 function updateNavOnLogin() {
   console.debug("updateNavOnLogin");
@@ -44,14 +37,6 @@ function navSubmitClick(evt){
 
 $body.on('click', '#nav-submit', navSubmitClick);
 
-
-// function navFavClick(evt) {
-//     console.debug("navFavClick", evt)
-//     hidePageComponents()
-//     $favoritesList.show()
-//   }
-  
-//   $body.on("click", "#nav-favorites", navFavClick)
   
 function navFavoritesClick(evt) {
     console.debug("navFavoritesClick", evt)
